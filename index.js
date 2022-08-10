15e652dab68750085c6d4a96573d630173e336da
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-08-10 16:04:53 -0300

    Atualização const port.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
